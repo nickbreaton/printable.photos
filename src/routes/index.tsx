af7a9577a4fbc4862e8a2dd8e85aa2ef7ddeca9a
Nick Breaton <nick@nickbreaton.com>
2023-05-26 22:23:22 -0400

    get some bin packing algo workign

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,8 @@
-import { component$, useSignal } from '@builder.io/qwik';
-import { jsPDF } from 'jspdf'
-import html2canvas from 'html2canvas'
-import { Signal } from '@builder.io/qwik';
+import { component$, useComputed$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
+import { Signal } from "@builder.io/qwik";
+import { MaxRectsPacker, Rectangle } from "maxrects-packer";
 
 const mockValues = {
   page: {
@@ -9,68 +10,129 @@ const mockValues = {
     height: 11,
     padding: 0.5,
   },
-  images:
-    [
-      {
-        src: '/photo.png',
-        width: 6,
-        height: 6
-      },
-      {
-        src: '/photo2.png',
-        width: 5,
-        height: 5
-      }
-    ],
-}
+  images: [
+    {
+      src: "/photo.png",
+      width: 5,
+      height: 5,
+    },
+    {
+      src: "/photo2.png",
+      width: 4,
+      height: 4,
+    },
+    // {
+    //   src: "/photo2.png?a",
+    //   width: 0.25,
+    //   height: 0.25,
+    // },
+  ],
+};
 
-const Page = component$(({ width, values, offscreen }: { width: string, values: typeof mockValues, offscreen?: Signal<HTMLElement> }) => {
-  const offscreenStyle = {
-    left: `-200vmax`,
-    top: `-200vmax`,
-    position: 'absolute',
-    pointerEvents: 'none'
+const Page = component$(
+  ({ width, values, offscreen }: { width: string; values: typeof mockValues; offscreen?: Signal<HTMLElement> }) => {
+    const offscreenStyle = {
+      left: `-200vmax`,
+      top: `-200vmax`,
+      position: "absolute",
+      pointerEvents: "none",
+    };
+
+    const offscreenAttributes = {
+      "aria-hidden": true,
+      ref: offscreen,
+    };
+
+    const bins = useComputed$(() => {
+      const packer = new MaxRectsPacker(values.page.width, values.page.height, 0.25, {
+        allowRotation: true,
+        smart: false, // why doesn't smart work?
+        border: 0.5,
+      });
+      packer.addArray(
+        values.images.map((image) => {
+          const rect = new Rectangle(image.width, image.height);
+          rect.data = image;
+          return rect;
+        })
+      );
+      return packer.bins.map((bin) => {
+        return {
+          rects: bin.rects.map((rect) => ({
+            width: rect.width,
+            height: rect.height,
+            x: rect.x,
+            y: rect.y,
+            rotate: rect.rot,
+            data: rect.data,
+          })),
+        };
+      });
+    });
+
+    useVisibleTask$(() => {
+      console.log(bins.value);
+    });
+
+    return (
+      <div
+        {...(offscreen ? offscreenAttributes : {})}
+        style={{
+          display: "inline-block",
+          width,
+          aspectRatio: `${values.page.width} / ${values.page.height}`,
+          background: "white",
+          minHeight: 0,
+          position: "relative",
+          ...(offscreen ? offscreenStyle : {}),
+        }}
+      >
+        {bins.value[0].rects.map((rect) => {
+          return (
+            <img
+              key={rect.data.src}
+              src={rect.data.src}
+              style={{
+                position: "absolute",
+                left: `calc(${rect.x} / ${values.page.width} * 100%)`,
+                marginTop: `calc(${rect.y} / ${values.page.width} * 100%)`, // use margin for percentage of width
+                width: `calc(${rect.width} / ${values.page.width} * 100%)`,
+                aspectRatio: `${rect.width} / ${rect.height}`,
+              }}
+            />
+          );
+        })}
+      </div>
+    );
   }
-  const offscreenAttributes = {
-    'aria-hidden': true,
-    ref: offscreen
-  }
-  return <div
-    {...(offscreen ? offscreenAttributes : {})}
-    style={{
-      display: 'inline-block',
-      width,
-      aspectRatio: `${values.page.width} / ${values.page.height}`,
-      // padding: `calc(${values.page.padding} / ${values.page.width} * 100%)`,
-      background: 'white',
-      minHeight: 0,
-      ...(offscreen ? offscreenStyle : {})
-    }}>
-    {values.images.map(image => {
-      return <img key={image.src} src={image.src} style={{ width: `calc(${image.width} / ${values.page.width} * 100%)`, aspectRatio: `${image.width} / ${image.height}` }} />
-    })}
-  </div>
-})
+);
 
 export default component$(() => {
-  const content = useSignal<HTMLDivElement>(null!)
+  const content = useSignal<HTMLDivElement>(null!);
+  // useVisibleTask$(() => {
 
+  // });
   return (
     <>
-      <button style="margin-bottom: 1rem;" onClick$={async () => {
-        const doc = new jsPDF({
-          unit: 'in',
-          orientation: 'p',
-          format: [8.5, 11]
-        });
-        const canvas = await html2canvas(content.value!)
-        doc.addImage(canvas, 0, 0, 8.5, 11)
-        doc.save('canvas.pdf')
-      }}>Download</button>
+      <button
+        style="margin-bottom: 1rem;"
+        onClick$={async () => {
+          const doc = new jsPDF({
+            unit: "in",
+            orientation: "p",
+            format: [8.5, 11],
+          });
+          const canvas = await html2canvas(content.value!);
+          doc.addImage(canvas, 0, 0, 8.5, 11);
+          doc.save("canvas.pdf");
+        }}
+      >
+        Download
+      </button>
 
       <div>
-        <Page width='80%' values={mockValues} />
-        <Page width='8.5in' values={mockValues} offscreen={content} />
+        <Page width="80%" values={mockValues} />
+        <Page width="8.5in" values={mockValues} offscreen={content} />
       </div>
     </>
   );
