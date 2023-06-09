5e3efde8232914f789b90dba81d846cfea896fff
Nick Breaton <nick@nickbreaton.com>
2023-06-09 19:33:17 -0400

    make everything client side for now

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,15 @@
-import { component$, Slot } from '@builder.io/qwik';
+import { component$, Slot, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 
 export default component$(() => {
+  const render = useSignal(false);
+
+  useVisibleTask$(() => {
+    render.value = true;
+  });
+
+  if (!render.value) {
+    return null;
+  }
+
   return <Slot />;
 });
