0f2414c485088249169f1acf8ebaa59574508135
Nick Breaton <nick@nickbreaton.com>
2026-05-03 10:35:48 -0400

    Allow certain image types only

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -8,6 +8,7 @@ export function FileInput(props: JSX.InputHTMLAttributes<HTMLInputElement>) {
     <Input
       {...props}
       type="file"
+      accept=".jpg,.jpeg,.png,.heic"
       class={cn(
         "file:mr-3 file:h-7 file:rounded-none file:border-0 file:bg-transparent file:px-0 file:text-sm file:font-medium file:text-foreground",
         props.class,
