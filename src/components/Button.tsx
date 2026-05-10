412bfc6a9b9202bbd5c9ffb96791a418057e01b9
Nick Breaton <nick@nickbreaton.com>
2026-05-10 01:38:50 -0400

    Add pressed state to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export function Button(props: JSX.ButtonHTMLAttributes<HTMLButtonElement>) {
     <button
       {...props}
       class={cn(
-        "inline-flex h-9 items-center justify-center border border-primary bg-primary px-3 py-2 text-sm font-medium text-primary-foreground outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
+        "inline-flex h-9 items-center justify-center border border-primary bg-primary px-3 py-2 text-sm font-medium text-primary-foreground outline-none active:translate-y-px focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
         props.class,
       )}
     />
